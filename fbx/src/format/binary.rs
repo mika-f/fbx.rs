73b84ee26e259f5a902096edb9ef2a519ea7114c
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-03-31 19:29:23 +0900

    feat: read fbx version

--- fbx/src/format/binary.rs
+++ fbx/src/format/binary.rs
@@ -14,16 +14,27 @@ impl BinaryFBX {
         BinaryFBX { reader: BinaryReader::new(reader), version: None }
     }
 
+    fn read_version(&mut self) {
+        let num = self.reader.read_u32_le();
+
+        let major = num / 1000;
+        let minor = (num - major * 1000) / 100;
+
+        self.version = Some(Version::from(major as u16, minor as u16))
+    }
+
 }
 
 impl base::BaseFBX for BinaryFBX {
     fn read(&mut self) {
+        self.read_version(); // 4 bytes
     }
 }
 
 impl Debug for BinaryFBX {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("BinaryFBX")
+            .field("version", &self.version)
             .finish()
     }
 }
