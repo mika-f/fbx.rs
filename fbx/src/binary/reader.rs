2b61172d981a80921cacdae98f639cddd5dbe2b4
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-04-01 22:57:35 +0900

    feat: support to read binary with any types

--- fbx/src/binary/reader.rs
+++ fbx/src/binary/reader.rs
@@ -25,6 +25,12 @@ impl BinaryReader {
         b[0] as char
     }
 
+    pub(crate) fn read_string(&mut self, length: usize) -> String {
+        let bytes = self.read_bytes_exact(length);
+
+        String::from_utf8(bytes).unwrap()
+    }
+
     pub(crate) fn read_boolean(&mut self) -> bool {
         let c = self.read_char();
 
@@ -39,15 +45,77 @@ impl BinaryReader {
         (c as u8) % 2 == 1 /* 0x00 or 0x01 */
     }
 
+    pub(crate) fn read_u8(&mut self) -> u8 {
+        self.read_bytes_exact(1)[0]
+    }
+
+    pub(crate) fn read_u16_be(&mut self) -> u16 {
+        let number = self.read_bytes_exact(2);
+        let bytes: [u8; 2] = number.try_into().unwrap();
+
+        u16::from_be_bytes(bytes)
+    }
+
+    pub(crate) fn read_u16_le(&mut self) -> u16 {
+        let number = self.read_bytes_exact(2);
+        let bytes: [u8; 2] = number.try_into().unwrap();
+
+        u16::from_le_bytes(bytes)
+    }
+
     pub(crate) fn read_u32_be(&mut self) -> u32 {
         let number = self.read_bytes_exact(4);
+        let bytes: [u8; 4] = number.try_into().unwrap();
 
-        ((number[0] as u32) << 24) + ((number[1] as u32) << 16) + ((number[2] as u32) << 8) + ((number[3] as u32) << 0)
+        u32::from_be_bytes(bytes)
     }
 
     pub(crate) fn read_u32_le(&mut self) -> u32 {
         let number = self.read_bytes_exact(4);
+        let bytes: [u8; 4] = number.try_into().unwrap();
 
-        ((number[0] as u32) << 0) + ((number[1] as u32) << 8) + ((number[2] as u32) << 16) + ((number[3] as u32) << 24)
+        u32::from_le_bytes(bytes)
+    }
+
+    pub(crate) fn read_u64_be(&mut self) -> u64 {
+        let number = self.read_bytes_exact(8);
+        let bytes: [u8; 8] = number.try_into().unwrap();
+
+        u64::from_be_bytes(bytes)
+    }
+
+    pub(crate) fn read_u64_le(&mut self) -> u64 {
+        let number = self.read_bytes_exact(8);
+        let bytes: [u8; 8] = number.try_into().unwrap();
+
+        u64::from_le_bytes(bytes)
+    }
+
+    pub(crate) fn read_f32_be(&mut self) -> f32 {
+        let number = self.read_bytes_exact(4);
+        let bytes: [u8; 4] = number.try_into().unwrap();
+
+        f32::from_be_bytes(bytes)
+    }
+
+    pub(crate) fn read_f32_le(&mut self) -> f32 {
+        let number = self.read_bytes_exact(4);
+        let bytes: [u8; 4] = number.try_into().unwrap();
+
+        f32::from_le_bytes(bytes)
+    }
+
+    pub(crate) fn read_f64_be(&mut self) -> f64 {
+        let number = self.read_bytes_exact(8);
+        let bytes: [u8; 8] = number.try_into().unwrap();
+
+        f64::from_be_bytes(bytes)
+    }
+
+    pub(crate) fn read_f64_le(&mut self) -> f64 {
+        let number = self.read_bytes_exact(8);
+        let bytes: [u8; 8] = number.try_into().unwrap();
+
+        f64::from_le_bytes(bytes)
     }
 }
