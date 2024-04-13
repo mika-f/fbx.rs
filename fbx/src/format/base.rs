f93ee22625136325de9d20ddcf4c9f9f9faf8117
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-04-14 01:14:37 +0900

    feat: add Node and Type (attribute) struct

--- fbx/src/format/base.rs
+++ fbx/src/format/base.rs
@@ -39,4 +39,33 @@ pub trait BaseFBX: Debug {
     fn read(&mut self);
 }
 
-pub trait Node: Debug {}
+pub trait Attribute: Debug {}
+
+#[derive(Debug)]
+pub struct Node {
+    name: String,
+    attributes: Vec<Type>,
+}
+
+impl Node {
+    pub fn new(name: String, attributes: Vec<Type>) -> Self {
+        Node { name, attributes }
+    }
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Bool(bool),
+    Int16(i16),
+    Int32(i32),
+    Int64(i64),
+    Float32(f32),
+    Float64(f64),
+    VecBool(Vec<bool>),
+    VecInt32(Vec<i32>),
+    VecInt64(Vec<i64>),
+    VecFloat32(Vec<f32>),
+    VecFloat64(Vec<f64>),
+    VecRaw(Vec<u8>),
+    String(String),
+}
