de563ea93aab42a327e738c6763a8a5a854cc24d
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-04-14 01:14:15 +0900

    impl Eq, Ord, Clone and Copy for Version struct

--- fbx/src/format/base.rs
+++ fbx/src/format/base.rs
@@ -1,9 +1,32 @@
+use std::cmp::Ordering;
 use std::fmt::Debug;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Ord, Clone, Copy)]
 pub struct Version {
     major: u16,
     minor: u16,
+}
+
+impl PartialOrd<Self> for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.major > other.major {
+            return Some(Ordering::Greater);
+        }
+
+        if self.major < other.major {
+            return Some(Ordering::Less);
+        }
+
+        if self.minor > other.minor {
+            return Some(Ordering::Greater);
+        }
+
+        if self.minor < other.minor {
+            return Some(Ordering::Less);
+        }
+
+        return Some(Ordering::Equal);
+    }
 }
 
 impl Version {
